1c2e7e8210b6b16e40d0cbb3f21854ddb7acb551
rafaeledas <rafaeledas@outlook.com>
2021-04-24 11:54:34 -0300

    master

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+
+            <html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=lexend:wght@500;600&display=swap" rel="stylwsheet" />
+
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
+    }
+
+}
